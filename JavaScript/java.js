331157ebcd6a9e7ce193e331790a360774a2d2c5
ImanolSayago <imanolsayago0@gmail.com>
2024-08-16 13:22:03 -0300

    hasta_ejercicio6

---
+++ JavaScript/java.js
@@ -0,0 +1,58 @@
+/*Ejercicio_1
+let nombre = "Imanol";
+console.log(nombre);
+
+/*ejercicio_2
+let num1= 10;
+let num2= 30;
+let suma = num1 + num2;
+console.log(suma);
+
+/*ejercicio_3
+const pi= Math.PI;
+console.log(pi);
+
+/*ejercicio_4
+let nombres= prompt("Ingrese el nombre asi tiene un saludo personalizado");
+
+function saludar (variable)
+{
+    return console.log("Buenas tardes", variable,"como estas?");
+}
+
+let saludo = saludar(nombres);
+console.log(saludo);*/
+
+/*ejercicio_5
+let numeropar= prompt("Ingrese un numero, si es par retornara true, si es impar false");
+
+function esPar(par)
+{
+    let pares = false;
+    if(par%2==0)
+    {
+        pares= true;
+    }
+    return pares;
+}
+
+let pares= esPar(numeropar);
+
+if(pares==true)
+{
+    let espar1= "El numero es par";
+    console.log(espar1);
+}
+else
+{
+    let noespar= "El numero no es par";
+    console.log(noespar);
+}*/
+
+/*ejercicio_6*/
+
+
+function sumarArray()
+{
+
+}
