16326b6ce5216bfe1813bcd08232aa8696c0e95c
ImanolSayago <imanolsayago0@gmail.com>
2024-08-20 14:13:21 -0300

    terminado

--- JavaScript/java.js
+++ JavaScript/java.js
@@ -1,12 +1,12 @@
+/*           EJERCICIO 1
 
-/*Ejercicio_1
 let nombre = "Imanol";
 console.log(nombre);
 
 
 
 
-/*ejercicio_2
+/*              EJERCICIO 2
 let num1= 10;
 let num2= 30;
 let suma = num1 + num2;
@@ -15,7 +15,7 @@ console.log(suma);
 
 
 
-/*ejercicio_3
+/*              EJERCICIO 3
 const pi= Math.PI;
 console.log(pi);
 
@@ -23,7 +23,7 @@ console.log(pi);
 
 
 
-/*ejercicio_4
+/*              EJERCICIO 4
 let nombres= prompt("Ingrese el nombre asi tiene un saludo personalizado");
 
 function saludar (variable)
@@ -40,7 +40,9 @@ console.log(saludo);*/
 
 
 
-/*ejercicio_5
+/*                  EJERCICIO 5
+
+
 let numeropar= prompt("Ingrese un numero, si es par retornara true, si es impar false");
 
 function esPar(par)
@@ -68,10 +70,7 @@ else
 
 
 
-
-
-
-/*ejercicio_6
+/*              EJERCICIO 6
 
 let  arreglo =new Array(5);
 
@@ -105,9 +104,7 @@ console.log("La suma del arreglo es: ",sumatoria);*/
 
 
 
-
-
-/*Ejercicio_7
+/*              EJERCICIO 7
 
 const persona1 =
 {
@@ -126,7 +123,7 @@ console.log("Profesion: ",persona1.profesion);*/
 
 
 
-/*Ejercicio_8
+/*              EJERCICIO 8
 
 function Producto (nombre, precio, stock)
 {
@@ -153,7 +150,7 @@ console.log("Stock producto: ",producto2.stock);*/
 
 
 
-/*Ejercicio_9
+/*              EJERCICIO 9
 
 function Pelicula(titulo, director, anio )
 {
@@ -182,7 +179,7 @@ console.log("Año pelicula :",pelicula2.anio);*/
 
 
 
-/*Ejercicio 10*/
+/*                  EJERCICIO 10
 
 /*tambien se puede hacer asi: let arrayfrutas=["manzana", "banana"," naranja"];*/
 //let arrayFrutas = new Array (3);
@@ -197,7 +194,7 @@ console.log("Año pelicula :",pelicula2.anio);*/
 
 
 
-/*ejercicio_11*/
+/*              EJERCICIO 11
 
 /*con push se agrega un elemento al final del arreglo*/
 //arrayFrutas.push("Mandarina");
@@ -208,7 +205,7 @@ console.log("Año pelicula :",pelicula2.anio);*/
 
 
 
-/*ejercicio_12
+/*              EJERCICIO 12
 
 let arregloNUM= [1,2,3,4,5,6,7,8,9,10];
 
@@ -232,10 +229,7 @@ console.log(arreglopares1);*/
 
 
 
-
-
-
-/*ejercicio_13
+/*              EJERCICIO 13
 
 function crearPersona(nombre,edad)
 {
@@ -255,7 +249,7 @@ console.log("Nombre: ",persona1.nombreP);
 console.log("Edad: ",persona1.edadP);
 
 
-/*Ejercicio_14
+/*          EJERCICIO 14
 
 function obtenerinfo(Persona)
 {
@@ -265,7 +259,9 @@ function obtenerinfo(Persona)
 let informacion = obtenerinfo(persona1);
 console.log(informacion);*/
 
-/*ejercicio_15
+
+
+/*          EJERCICIO 15
 
 let arreglonro=[1,2,3,4,5];
 function duplicarnrosarray(arreglo)
@@ -286,7 +282,7 @@ let arregloduplicado= duplicarnrosarray(arreglonro);
 console.log(arreglonro);
 console.log(arregloduplicado);*/
 
-/*ejercicio_16
+/*          EJERCICIO 16
 
 function invertirCadena(cadena)
 {
@@ -303,7 +299,7 @@ console.log(texto);
 console.log("Mostrando el texto invertido");
 console.log(textoInv);*/
 
-/*ejercicio_17
+/*          EJERCICIO 17
 
 let arreglopalabras= ["labo","laboratorio","otra"];
 function filtrarporLongitud(nro1, arreglo)
@@ -328,7 +324,7 @@ console.log(arreglopalabras);
 console.log("Mostrando el arreglo de palabras nuevo(el filtro en este caso es 8 letras)");
 console.log(arreglonuevopalabras);*/
 
-/*Ejercicio_18
+/*          EJERCICIO 18
 
 
 
@@ -353,7 +349,9 @@ let estudiante3= crearEstudiante("Nicolas",20,8.7);
 let arregloestudiantes= [estudiante1,estudiante2,estudiante3];
 console.log(arregloestudiantes);
 
-/*ejercicio19
+
+
+/*          EJERCICIO 19
 
 function buscarestudiantePorNombre(arregloestudiante,nombre)
 {
@@ -387,7 +385,7 @@ else
 }
 
 
-/*ejercicio_20
+/*          EJERCICIO 20
 let promedio_general;
 
 
@@ -408,4 +406,328 @@ function promedioTodosAlumnos(arregloestudiantess)
  console.log("El promedio de la suma de todos los alumnos esde: ",promedio_general);
 
  */
+
+/*          EJERCICIO 21
+function crearcoche(marcas,modelos,anios,valorarranques)
+{
+    let coche =
+    {
+        marca:marcas,
+        modelo:modelos,
+        anio:anios,
+        valorarranque:false,
+    };
+    return coche;
+}
+
+let coche1= crearcoche("Chevrolet","onix",2020);
+let coche2= crearcoche("Ford","fiesta",2023);
+let coche3 = crearcoche("Chevrolet","Tracker",2024);
+
+function arrancar(coches)
+{
+    if(coches.valorarranque==false)
+    {
+        coches.valorarranque=true;
+    }
+}
+
+console.log("Mostrando coche antes de ser arrancado: ");
+console.log("Marca auto: ",coche1.marca);
+console.log("Modelo coche: ",coche1.modelo);
+console.log("Año coche: ",coche1.anio);
+if(coche1.valorarranque==false)
+{
+    console.log("El auto esta apagado");
+}
+else
+{
+    console.log("El auto esta encendido");
+}
+
+
+arrancar(coche1);
+
+console.log("Prendiendo el auto...");
+
+if(coche1.valorarranque==false)
+    {
+        console.log("El auto esta apagado");
+    }
+    else
+    {
+        console.log("El auto esta encendido");
+    }*/
+
+        /*EJERCICIO 22
+
+function crearHumano(nombres,edadd)
+{
+    let humano=
+    {
+        nombre:nombres,
+        edad:edadd,
+        saludar: function()
+        {
+            console.log(`Hola soy ${this.nombre} y tengo ${this.edad} años`);
+        }
+    };
+    return humano;
+}
+
+let humano1= crearHumano("Imanol",20);
+humano1.saludar();
+
+let humano2= crearHumano("Lucas",23);
+humano2.saludar();
+
+let humano3= crearHumano("Nicolas",20);
+humano3.saludar();*/
+
+
+            /*EJERCICIO 24
+
+
+function carrito()
+{
+   this.carro=[];
+
+   this.agregarproducto = function(producto)
+   {
+    this.carro.push(producto);
+   }
+
+   this.mostrarproducto= function()
+   {
+    console.log(this.carro);
+   }
+   
+}
+
+let carro= new carrito();
+
+carro.agregarproducto("manzana");
+carro.agregarproducto("leche");
+carro.agregarproducto("carne");
+
+carro.mostrarproducto();*/
+
+
+
+/*                          EJERCICIO 25
+
+function crearest(nombres,edaddd)
+{
+    let estud=
+    {
+        nombre:nombres,
+        edad:edaddd,
+    };
+    return estud;
+}
+
+let estudiante1=crearest("Imanol",20);
+let estudiante2= crearest("Lucas",23);
+let estudiante3= crearest("Nicolas",20);
+
+let arrest=[estudiante1,estudiante2,estudiante3];
+
+console.log("Arreglo completo: ");
+console.log(arrest);
+
+function buscaryeliminarest(arreglo, nombrebuscado)
+{
+    let flag=0;
+    let i = 0;
+    while(i<arreglo.length && flag==0)
+    {
+        let nombre = arreglo[i].nombre;
+        if(nombre == nombrebuscado)
+        {
+            flag = 1;
+            arreglo.splice(i,1);
+        }
+        else
+        {
+            i++;
+        }
+        
+    }
+}
+
+console.log("Arreglo eliminando a Imanol");
+buscaryeliminarest(arrest,"Imanol");
+console.log(arrest);*/
+
+/*                  EJERCICIO 26
+
+function crearproducto(nombres, precios)
+{
+    let producto = 
+    {
+        nombreproducto:nombres,
+        precio:precios,
+    };
+    return producto;
+}
+
+producto1= crearproducto("carne",20);
+producto2= crearproducto("pollo",10);
+producto3= crearproducto("lentejas",8);
+producto4= crearproducto("manzana",4);
+producto5= crearproducto("naranja",2);
+producto6= crearproducto("banana",1);
+
+
+function carrito()
+{
+    this.carro=[];
+    this.agregarprodu= function agregar(producto)
+    {
+        this.carro.push(producto);
+    }
+    this.sumartodo= function sumartodo()
+    {
+        let suma=0;
+        for(let i = 0; i<this.carro.length;i++)
+        {
+           suma= suma + this.carro[i].precio;
+        }
+        console.log("El precio de todo el carro es de: ",suma);
+    }
+}
+
+let carronuevo = new carrito();
+
+carronuevo.agregarprodu(producto1);
+carronuevo.agregarprodu(producto2);
+carronuevo.agregarprodu(producto3);
+
+carronuevo.sumartodo();*/
+
+
+
+
+/*                  EJERCICIO 28
+
+function crearusuario(nombres,edades,dnis)
+{
+    let usuario = 
+    {
+        nombre: nombres,
+        edad:edades,
+        dni:dnis,
+    }
+    return usuario;
+}
+
+let usuario1= crearusuario("Imanol",20,44932628);
+
+function crearcuentaBancaria(usu)
+{
+    let cuenta = 
+    {
+        usuario: usu,
+        totalcuenta:0,
+        depositar: function depositardinero(totaldepo)
+        {
+            console.log("Depositando:",totaldepo,"en su cuenta")
+            this.totalcuenta= this.totalcuenta+totaldepo;
+        },
+
+    
+        retirar: function retirardinero(montoretirar)
+        {
+            
+            if(montoretirar<this.totalcuenta)
+            {
+                console.log("Usted retiro:",montoretirar, "\nDinero retirado con exito");
+                this.totalcuenta=this.totalcuenta-montoretirar;
+            }
+            else
+            {
+                console.log("Usted esta tratando de retirar: ",montoretirar,"de su cuenta");
+                console.log("No se puede retirar esa suma de dinero, no cuenta con los fondos suficientes");
+            }
+        },
+
+        mostrarsaldo: function mostrarsaldo()
+        {
+            console.log("Su saldo en la cuenta es de: ",this.totalcuenta);
+        }
+    }
+return cuenta;
+}
+
+let cuentabancaria1 = crearcuentaBancaria(usuario1);
+
+console.log("Datos de la cuenta:");
+console.log("Nombre usuario: ",cuentabancaria1.usuario.nombre);
+console.log("Edad: ",cuentabancaria1.usuario.edad);
+console.log("DNI: ",cuentabancaria1.usuario.dni);
+
+cuentabancaria1.mostrarsaldo();
+
+///agrego 30 a la cuenta///
+
+cuentabancaria1.depositar(30);
+
+cuentabancaria1.mostrarsaldo();
+
+
+///retiro 20 de la cuenta///
+cuentabancaria1.retirar(20);
+
+///Muestra de la cuenta///
+cuentabancaria1.mostrarsaldo();
+
+///tratando de retirar mas de lo que tengo en la cuenta///
+
+cuentabancaria1.retirar(30);*/
+
+
+
+
+/*                           EJERCICIO 29
+
+
+function crearautor(nombreautor, edadautor)
+{
+    let autor=
+    {
+        nombre: nombreautor,
+        edad:edadautor,
+        infoautor: function infoautor()
+        {
+            console.log("Nombre autor: ",this.nombre);
+            console.log("Edad autor: ",this.edad);
+        },
+    }
+    return autor;
+}
+
+let autor1= crearautor("PedroAutor",47);
+
+
+function crearlibro(titulos, autores,paginas)
+{
+    let libro=
+    {
+        Titulolibro:titulos,
+        autor:autores,
+        paginaslibro:paginas,
+        mostrarinfo: function mostrarinfo()
+        {
+            console.log("Informacion del libro");
+            console.log("Titulo del libro: ",this.Titulolibro);
+            console.log("Informacion sobre el autor");
+            this.autor.infoautor();
+            console.log("Paginas totales:",this.paginaslibro);
+        }
+    }
+    return libro;
+}
+
+let libro = crearlibro("Mosquita muerta",autor1,300);
+libro.mostrarinfo();*/
 
